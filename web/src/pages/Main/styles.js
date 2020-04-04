31d96193fe8a263437ad50ea2ccae0c94c11df77
Guilherme M. Kogus <guilherme.mkogus@gmail.com>
2020-04-03 23:32:48 -0300

    styled components

---
+++ web/src/pages/Main/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Title = styled.div`
+  font-size: 24px;
+  text-align: center;
+  color: #7159c1;
+  font-family: Arial, Helvetica, sans-serif;
+  p {
+    font-size: 14px;
+    color: #333;
+  }
+`;
